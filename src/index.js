b1c0faeb4d5908a1786410829002e8d8c2000fed
iskonplus <58971909+iskonplus@users.noreply.github.com>
2024-02-04 14:11:14 +0100

    feat: add exit user

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import { getUserName } from './cli/args.js';
+import readline from 'readline/promises'
+import { exitUser } from './userManager/exitUser.js';
+
 const userName = getUserName();
 
 console.log(`Welcome to the File Manager, ${userName}!`);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+
+rl.on('line', async (userArguments) => {
+
+    if (userArguments === '.exit') {
+        exitUser(userName);
+        rl.close();
+        return;
+    }
+});
+
+
+
+rl.on('SIGINT', async () => {
+    exitUser(userName);
+    rl.close();
+})
---
+++ src/userManager/exitUser.js
@@ -0,0 +1,3 @@
+export const exitUser = (userName) => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+}
