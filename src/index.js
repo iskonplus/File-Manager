c968c6f035f6f2086219b0b50375b647e8bc1259
iskonplus <58971909+iskonplus@users.noreply.github.com>
2024-02-05 21:59:58 +0100

    feat: add commond cd & ls

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { exitUser } from './userManager/exitUser.js';
 import { outputCurrentPath } from './userManager/outputCurrentPath.js';
 import os from 'node:os';
 import { commandUp } from './navi/up.js';
+import { commandCd } from './navi/cd.js';
+import { commandLs } from './navi/ls.js';
+
 
 const userName = getUserName();
 let currentPath = os.homedir();
@@ -27,7 +30,22 @@ rl.on('line', async (userArguments) => {
         currentPath = commandUp(currentPath);
         outputCurrentPath(currentPath);
         return;
-    } else {
+    }
+    if (userArguments.startsWith('cd')) {
+        rl.pause();
+         currentPath = await commandCd(userArguments, currentPath);
+        outputCurrentPath(currentPath);
+        rl.resume();
+        return;
+    }
+    if (userArguments === 'ls') {
+        rl.pause();
+        const contant = await commandLs(currentPath)
+        console.table(contant);
+        outputCurrentPath(currentPath);
+        rl.resume();
+        return;
+    }else {
         console.log(`Invalid input`);
         outputCurrentPath(currentPath);
     }
