6ed33807b91e1790e9a819cb2ae725865765fc45
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-29 16:24:01 +0100

    feat: add file and directory validation to delete command

--- src/fs/deletePath.js
+++ src/fs/deletePath.js
@@ -1,17 +1,24 @@
 import path from 'path';
 import fs from 'fs';
 import { errOperation } from '../errors/errOperation.js';
+import { getNormalizePath, validateDirectory, validateFile } from '../utils/utils.js';
 
 
 
 
 export const deletePath = async (currentPath, fileName) => {
-
-    const filePath = path.join(currentPath, fileName);
-    
     try {
+
+        const filePath = await getNormalizePath(currentPath, fileName);
+        const basePath = path.dirname(filePath);
+        const baseName = path.basename(fileName);
+
+        await validateDirectory(basePath);
+        await validateFile(filePath);
+    
+    
         await fs.promises.unlink(filePath);
-        console.log(`File ${fileName} has been deleted from ${currentPath}`);
+        console.log(`File ${baseName} has been deleted from ${basePath}`);
 
     } catch {
         const error = errOperation();
