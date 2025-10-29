fefdc51b62e1eee0bb98da0b3ba01203c592ec5d
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-29 17:48:47 +0100

    feat: add file and directory validation to hash command

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -3,18 +3,26 @@ import { createReadStream } from 'fs';
 import { pipeline } from 'stream/promises';
 import { createHash } from 'crypto';
 import { errOperation } from '../errors/errOperation.js';
+import { getNormalizePath, validateDirectory, validateFile } from '../utils/utils.js';
 
 
 export const calcHash = async (currentPath, userPath) => {
-    console.log(' ');
-
+    
     try {
-        const filePath = path.join(currentPath, userPath);
+        const filePath = await getNormalizePath(currentPath, userPath);
+        const baseName = path.basename(filePath);
+        const baseDir = path.dirname(filePath);
+
+        await validateDirectory(baseDir)
+        await validateFile(filePath);
+
         const hash = createHash('sha256');
-
         await pipeline(createReadStream(filePath), hash);
-
+        
         const result = hash.digest('hex');
+
+        console.log(' ');
+        console.log(`Hash has been calculated of: ${baseName}`);
         console.log(`SHA256 hash: ${result}`);
 
     } catch {
